36a685aa3f0fadc3d219b97e0134fd8d02307db0
Русаков Андрей <70331127+ochenudivlen@users.noreply.github.com>
2022-04-07 23:18:29 +0300

    Add files via upload

--- Monte-Carlo.cpp
+++ Monte-Carlo.cpp
@@ -30,6 +30,8 @@ int main()
 
 	std::vector<int> vec(N * Size);
 
+	std::cout << "ball:" << endl;
+
 	for (int i = 0; i < N * Size; ++i)
 	{
 		if (sqrt((vec1[i]) * (vec1[i]) + (vec2[i]) * (vec2[i]) + (vec3[i]) * (vec3[i])) < R * 1.0)
@@ -56,10 +58,11 @@ int main()
 		);
 	}
 
-	std::cout << "ball:" << endl;
 	std::cout << "Etalon: " << 4.0 / 3.0 * M_PI * R * R * R << endl;
+
 	double sum = 0;
 	for (auto& result : f) sum = sum + result.get();
+
 	std::cout << "Sum:    " << sum << endl;
 	std::cout << "Val:    " << (sum * 8 * R * R * R) / (N * Size * 1.0) << endl;
 	std::cout << "Pi:     " << (3 * (sum * 8 * R * R * R) / (N * Size * 1.0)) / (4 * R * R * R * 1.0) << endl;
